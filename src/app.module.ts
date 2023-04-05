fa40dec0c0fe12a644f57f7d9046c0977a244245
BobrikAU <kirbob0@yandex.ru>
2023-04-05 18:07:11 +0300

    feature: make database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // process.env.DATABASE_URL || 'localhost',
+      port: 5432, // parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: 'student', // process.env.DATABASE_USERNAME || 'test',
+      password: 'student', // process.env.DATABASE_PASSWORD || 'test',
+      database: 'kupipodariday', //process.env.DATABASE_NAME || 'test',
+      synchronize: false,
+      // logging: true,
+      entities: [],
+      // subscribers: [],
+      // migrations: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
