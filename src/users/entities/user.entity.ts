cfaad62ee8f9ed0f91522b96393d37b8381fd02a
BobrikAU <kirbob0@yandex.ru>
2023-04-06 21:02:24 +0300

    feature: make wish schema

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -4,7 +4,10 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   Column,
+  OneToMany,
 } from 'typeorm';
+import { Length, IsNotEmpty, IsUrl, IsEmail } from 'class-validator';
+import { Wisch } from '../../wisches/entities/wisch.entity';
 
 @Entity()
 export class User {
@@ -12,25 +15,32 @@ export class User {
   id: number;
 
   @Column('varchar', { length: 30, unique: true, nullable: true })
+  @Length(2, 30)
+  @IsNotEmpty()
   username: string;
 
   @Column('varchar', {
     length: 200,
     default: 'Пока ничего не рассказал о себе',
   })
+  @Length(2, 200)
   about: string;
 
   @Column('varchar', { default: 'https://i.pravatar.cc/300' })
+  @IsUrl()
   avatar: string;
 
-  @Column('varchar', { unique: true })
+  @Column('varchar', { unique: true, nullable: false })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @Column('varchar', { nullable: true })
+  @IsNotEmpty()
   password: string;
 
-  @Column()
-  wishes: number[];
+  @OneToMany(() => Wisch, (wisch) => wisch.owner)
+  wishes: Wisch[];
 
   @Column()
   offers: number[];
--- src/wisches/entities/wisch.entity.ts
+++ src/wisches/entities/wisch.entity.ts
@@ -1,1 +1,57 @@
-export class Wisch {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  ManyToOne,
+} from 'typeorm';
+import { IsNotEmpty, IsUrl, Length } from 'class-validator';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Wisch {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 250, nullable: false })
+  @Length(1, 250)
+  @IsNotEmpty()
+  name: string;
+
+  @Column({ nullable: false })
+  @IsUrl()
+  @IsNotEmpty()
+  link: string;
+
+  @Column({ nullable: false })
+  @IsUrl()
+  @IsNotEmpty()
+  image: string;
+
+  @Column('decimal', { precision: 2, nullable: false })
+  @IsNotEmpty()
+  price: number;
+
+  @Column('decimal', { precision: 2 })
+  raised: number;
+
+  @ManyToOne(() => User, (user) => user.wishes)
+  owner: User;
+
+  @Column('varchar', { length: 1024 })
+  @Length(1, 1024)
+  description: string;
+
+  @Column()
+  offers: number[];
+
+  @Column('integer')
+  copied: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
