c445251fc6ebdf4f48e16cceeb858a43abaccb57
BobrikAU <kirbob0@yandex.ru>
2023-04-06 16:26:01 +0300

    feature: make users schema

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,46 @@
-export class User {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 30, unique: true, nullable: true })
+  username: string;
+
+  @Column('varchar', {
+    length: 200,
+    default: 'Пока ничего не рассказал о себе',
+  })
+  about: string;
+
+  @Column('varchar', { default: 'https://i.pravatar.cc/300' })
+  avatar: string;
+
+  @Column('varchar', { unique: true })
+  email: string;
+
+  @Column('varchar', { nullable: true })
+  password: string;
+
+  @Column()
+  wishes: number[];
+
+  @Column()
+  offers: number[];
+
+  @Column()
+  wishlist: number[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
